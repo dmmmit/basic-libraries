c2fb6be673e729e61fd70e6bb8e66716c27c62f9
dimau <dima.urosov@gmail.com>
2024-02-18 16:34:28 +0300

    v0.01 reload

--- prj.lab/complex/complex.cpp
+++ prj.lab/complex/complex.cpp
@@ -3,37 +3,130 @@
 
 Complex Complex::operator-() const noexcept { return Complex(-re, -im); }
 
-bool Complex::operator==(const Complex& rhs) const noexcept { return true; }
-bool Complex::operator!=(const Complex& rhs) const noexcept { return true; }
+bool Complex::operator==(const Complex &rhs) const noexcept { return true; }
 
-Complex& Complex::operator+=(const Complex& rhs) noexcept { return *this; }
-Complex& Complex::operator+=(const double rhs) noexcept { return operator+=(Complex(rhs)); }
+bool Complex::operator!=(const Complex &rhs) const noexcept { return true; }
 
-Complex& Complex::operator-=(const Complex& rhs) noexcept { return *this; }
-Complex& Complex::operator-=(const double rhs) noexcept { return operator-=(Complex(rhs)); }
+Complex &Complex::operator+=(const Complex &rhs) noexcept {
+    re += rhs.re;
+    im += rhs.im;
+    return *this;
+}
 
-Complex& Complex::operator*=(const Complex& rhs) noexcept {  return *this; }
-Complex& Complex::operator*=(const double rhs) noexcept { return *this; }
+Complex &Complex::operator+=(const double rhs) noexcept {
+    re += rhs;
+    return *this;
+}
 
-Complex& Complex::operator/=(const Complex& rhs) { return *this; }
-Complex& Complex::operator/=(const double rhs) { return *this; }
+Complex &Complex::operator-=(const Complex &rhs) noexcept {
+    re -= rhs.re;
+    im -= rhs.im;
+    return *this;
+}
 
-std::ostream& Complex::WriteTo(std::ostream& ostrm) const noexcept { return ostrm; }
-std::istream& Complex::ReadFrom(std::istream& istrm) noexcept { return istrm; }
+Complex &Complex::operator-=(const double rhs) noexcept {
+    re -= rhs;
+    return *this;
+}
+
+Complex &Complex::operator*=(const Complex &rhs) noexcept {
+    double res_Re = re * rhs.re - im * rhs.im;
+    double res_Im = im * rhs.re + re * rhs.im;
+    re = res_Re;
+    im = res_Im;
+    return *this;
+}
+
+Complex &Complex::operator*=(const double rhs) noexcept {
+    re *= rhs;
+    im *= rhs;
+
+    return *this;
+}
+
+Complex &Complex::operator/=(const Complex &rhs) {
+    double d = rhs.re * rhs.re + rhs.im * rhs.im;
+    double res_re = (re * rhs.re + im * rhs.im) / d;
+    double res_im = (im * rhs.re - re * rhs.im) / d;
+    re = res_re;
+    im = res_im;
+    return *this;
+}
+
+Complex &Complex::operator/=(const double rhs) {
+    re /= rhs;
+    im /= rhs;
+
+    return *this;
+}
+
+std::ostream &Complex::WriteTo(std::ostream &ostrm) const noexcept { return ostrm; }
+
+std::istream &Complex::ReadFrom(std::istream &istrm) noexcept { return istrm; }
 
 
-Complex operator+(const Complex& lhs, const Complex& rhs) noexcept { return Complex(lhs) += rhs; }
-Complex operator+(const Complex& lhs, const double rhs) noexcept { return Complex(lhs) += rhs; }
-Complex operator+(const double lhs, const Complex& rhs) noexcept { return Complex(lhs) += rhs; }
+Complex operator+(const Complex &lhs, const Complex &rhs) noexcept {
+    return Complex(lhs.re + rhs.re, lhs.im + rhs.im);
+}
 
-Complex operator-(const Complex& lhs, const Complex& rhs) noexcept { return Complex(lhs) -= rhs; }
-Complex operator-(const Complex& lhs, const double rhs) noexcept { return Complex(lhs) -= rhs; }
-Complex operator-(const double lhs, const Complex& rhs) noexcept { return Complex(lhs) -= rhs; }
+Complex operator+(const Complex &lhs, const double rhs) noexcept {
+    return Complex(lhs.re + rhs, lhs.im);
+}
 
-Complex operator*(const Complex& lhs, const Complex& rhs) noexcept { return Complex(lhs) *= rhs; }
-Complex operator*(const Complex& lhs, const double rhs) noexcept { return Complex(lhs) *= rhs; }
-Complex operator*(const double lhs, const Complex& rhs) noexcept { return Complex(lhs) *= rhs; }
+Complex operator+(const double lhs, const Complex &rhs) noexcept {
+    return Complex(lhs + rhs.re, rhs.im);
+}
 
-Complex operator/(const Complex& lhs, const Complex& rhs) { return Complex(lhs) /= rhs; }
-Complex operator/(const Complex& lhs, const double rhs) { return Complex(lhs) /= rhs; }
-Complex operator/(const double lhs, const Complex& rhs) { return Complex(lhs) /= rhs; }
+Complex operator-(const Complex &lhs, const Complex &rhs) noexcept {
+    return Complex(lhs.re - rhs.re, lhs.im - rhs.im);
+}
+
+Complex operator-(const Complex &lhs, const double rhs) noexcept {
+    return Complex(lhs.re - rhs, lhs.im);
+}
+
+Complex operator-(const double lhs, const Complex &rhs) noexcept {
+    return Complex(lhs - rhs.re, rhs.im);
+}
+
+Complex operator*(const Complex &lhs, const Complex &rhs) noexcept {
+    double res_re = lhs.re * rhs.re - lhs.im * rhs.im;
+    double res_im = lhs.im * rhs.re + lhs.re * rhs.im;
+    return Complex(res_re, res_im);
+}
+
+Complex operator*(const Complex &lhs, const double rhs) noexcept {
+    double res_re= lhs.re * rhs;
+    double res_im =lhs.im * rhs;
+    return Complex(res_re, res_im);
+}
+
+Complex operator*(const double lhs, const Complex &rhs) noexcept {
+    double res_re = rhs.re * lhs;
+    double res_im = rhs.im * lhs;
+    return Complex(res_re, res_im);
+}
+
+Complex operator/(const Complex &lhs, const Complex &rhs) {
+
+    double d = rhs.re * rhs.re + rhs.im * rhs.im;
+    double res_re = (lhs.re * rhs.re + lhs.im * rhs.im) / d;
+    double res_im = (lhs.im * rhs.re - lhs.re * rhs.im) / d;
+
+    return Complex(res_re, res_im);
+}
+
+Complex operator/(const Complex &lhs, const double rhs) {
+    double res_re = lhs.re / rhs ;
+    double res_im = lhs.im / rhs;
+
+    return Complex(res_re, res_im);
+}
+
+Complex operator/(const double lhs, const Complex &rhs) {
+    double res_re = rhs.re / lhs;
+    double res_im = rhs.im / lhs;
+    return Complex(res_re, res_im);
+}
+
+
