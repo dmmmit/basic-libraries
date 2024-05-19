53a4c3ee37b09edec36edd3020494297da258b46
dimau <dima.urosov@gmail.com>
2024-05-20 00:30:21 +0300

    v0.2 final maybe

--- prj.lab/hashtable/hashtable.hpp
+++ prj.lab/hashtable/hashtable.hpp
@@ -41,7 +41,7 @@ public:
         const std::vector<Node*>& table;
 
     public:
-        size_t getIndex() {
+        size_t index_get() {
             return index;
         }
 
@@ -90,3 +90,99 @@ public:
         return Iterator(table.size(), nullptr, table);
     }
 };
+
+
+template <typename KeyType, typename ValueType>
+hashTable<KeyType, ValueType>::hashTable(size_t size) {
+    size_ = size;
+    table = std::vector<Node*>(size, nullptr);
+}
+template <typename KeyType, typename ValueType>
+hashTable<KeyType, ValueType>::hashTable() {
+    size_ = 60;
+    table = std::vector<Node*>(size_, nullptr);
+}
+
+template <typename KeyType, typename ValueType>
+hashTable<KeyType, ValueType>::~hashTable() {
+    for (Node* node : table) {
+        if (node != nullptr) {
+            deleteNodeSequence(node);
+        }
+    }
+}
+
+template <typename KeyType, typename ValueType>
+void hashTable<KeyType, ValueType>::delNode(Node* node) {
+    Node* current = node;
+    Node* prev = nullptr;
+    while (current != nullptr) {
+        prev = current;
+        current = current->next;
+        delete prev;
+    }
+}
+
+
+template <typename KeyType, typename ValueType>
+void hashTable<KeyType, ValueType>::insert(const KeyType& key, const ValueType& value) {
+    size_t hash = hash_function(key) % size_;
+    Node* current = table[hash];
+    Node* prev = nullptr;
+
+    while (current != nullptr && current->key != key) {
+        prev = current;
+        current = current->next;
+    }
+
+    if (current == nullptr) {
+        Node* newNode = new Node(key, value);
+        if (prev != nullptr) {
+            prev->next = newNode;
+        } else {
+            table[hash] = newNode;
+        }
+    } else {
+        throw std::invalid_argument("Key error");
+    }
+}
+
+
+template <typename KeyType, typename ValueType>
+bool hashTable<KeyType, ValueType>::contains(const KeyType& key) const {
+    size_t hash = hash_function(key) % size_;
+    Node* current = table[hash];
+
+    while (current != nullptr) {
+        if (current->key == key) {
+            return true;
+        }
+        current = current->next;
+    }
+
+    return false;
+}
+
+template <typename KeyType, typename ValueType>
+ValueType& hashTable<KeyType, ValueType>::operator[](const KeyType& key) {
+    size_t hash = hash_function(key) % size_;
+    Node* current = table[hash];
+    Node* prev = nullptr;
+
+    while (current != nullptr && current->key != key) {
+        prev = current;
+        current = current->next;
+    }
+
+    if (current == nullptr) {
+        Node* newNode = new Node(key, ValueType());
+        if (prev != nullptr) {
+            prev->next = newNode;
+        } else {
+            table[hash] = newNode;
+        }
+        return newNode->value;
+    }
+
+    return current->value;
+}
